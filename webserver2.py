1406465315b74a147a5ccf0ea34d0edd96693aee
DYLANSABIITI <dylansabiiti@gmail.com>
2020-09-08 12:04:26 +0300

    creating a new restaurant page

--- webserver2.py
+++ webserver2.py
@@ -6,7 +6,7 @@ from sqlalchemy import create_engine
 from sqlalchemy.orm import sessionmaker
 
 # Create session and connect to DB
-engine = create_engine('sqlite:///restaurantmenu.db')
+engine = create_engine('sqlite:///restaurantmenu.db', pool_pre_ping=True)
 Base.metadata.bind = engine
 DBSession = sessionmaker(bind=engine)
 session = DBSession()
@@ -16,7 +16,7 @@ app = Flask(__name__)
 @app.route("/restaurants", methods=["GET"])
 def restaurants():
     restaurants = session.query(Restaurant).all()
-    output += "<html><body>"
+    output = "<html><body>"
     for restaurant in restaurants:
         output += restaurant.name
         output += "</br>"
@@ -28,7 +28,8 @@ def restaurants():
 
 if __name__ == "__main__":
     try:
-        app.run(debug=True)
         print('Web server running...open localhost:8080/restaurants in your browser')
+        app.debug = True
+        app.run(host="localhost", port=8080)
     except KeyboardInterrupt:
         print('^C received, shutting down server')
